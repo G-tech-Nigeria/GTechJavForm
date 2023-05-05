fd0ede1615eeaea0f9b06c56087361eaf53d01c3
AleeyahAnjola <anjolaoluwami20@gmail.com>
2023-05-05 21:52:24 +0100

    second commit

---
+++ src/main/java/com/GTech/org/GTechJavFormApplication.java
@@ -0,0 +1,13 @@
+package com.GTech.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GTechJavFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GTechJavFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTech/org/GTechJavFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTech.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GTechJavFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
